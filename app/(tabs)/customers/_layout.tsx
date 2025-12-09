518727be5efec074ef5f6b4fd206db1cac364304
NikzGod <godxnikz@gmail.com>
2025-12-09 18:18:26 +0530

    Add files via upload

--- app/(tabs)/customers/_layout.tsx
+++ app/(tabs)/customers/_layout.tsx
@@ -1,1 +1,18 @@
+import { Stack } from 'expo-router';
 
+export default function CustomersLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: true,
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Customers",
+        }}
+      />
+    </Stack>
+  );
+}
