1960334259b5692aa69e163236b0253880d58a79
NikzGod <godxnikz@gmail.com>
2025-12-09 18:15:51 +0530

    Add files via upload

---
+++ app/(tabs)/(home)/_layout.tsx
@@ -0,0 +1,18 @@
+import { Stack } from 'expo-router';
+
+export default function HomeLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: true,
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Dashboard",
+        }}
+      />
+    </Stack>
+  );
+}
