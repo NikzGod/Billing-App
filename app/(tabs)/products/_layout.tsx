7ecbd74875bf21fcb3fb7e66343d477254fe9eb3
NikzGod <godxnikz@gmail.com>
2025-12-09 18:24:44 +0530

    Add files via upload

--- app/(tabs)/products/_layout.tsx
+++ app/(tabs)/products/_layout.tsx
@@ -1,1 +1,18 @@
+import { Stack } from 'expo-router';
 
+export default function ProductsLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: true,
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Products",
+        }}
+      />
+    </Stack>
+  );
+}
